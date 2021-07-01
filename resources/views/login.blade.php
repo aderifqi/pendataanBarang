4d113aec249aa3b4d108f45f9243a32ed7be575c
mnihax <whoisichlas@gmail.com>
2021-07-01 12:23:10 +0700

    update login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <link rel="stylesheet" href={{ asset('css/style.css') }}>
+	<title>Login</title>
+</head><l
+<body>
+<div class="login">
+    <h2 class="login-header">Login</h2>
+        <form class="login-container">
+            <p>
+                <input type="email" placeholder="Email">
+            </p>
+            <p>
+                <input type="password" placeholder="Password">
+            </p>
+            <p>
+                <input type="submit" value="Log in">
+            </p>
+        </form>
+</div>
+</body>
+</html>
